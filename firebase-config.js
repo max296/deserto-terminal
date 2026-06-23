0a530eb4d519b00aaecadd03b41d817600cf6b26
max296 <max@zirok.com>
2026-06-23 11:11:42 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+// ============================================================
+const firebaseConfig = {
+  apiKey: "AIzaSyB6e6GiyI4zwpGlXflnkLS-X6jaEabRmcc",
+  authDomain: "deserto-terminal.firebaseapp.com",
+  databaseURL: "https://deserto-terminal-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "deserto-terminal",
+  storageBucket: "deserto-terminal.firebasestorage.app",
+  messagingSenderId: "407431125870",
+  appId: "1:407431125870:web:e77eacc2d4c24104537f7f"
+};
+
+// ------------------------------------------------------------
+const SESSION_KEY = "deserto-mesa-01";
+
+// ------------------------------------------------------------
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+const sessionRef = db.ref("sessions/" + SESSION_KEY);
